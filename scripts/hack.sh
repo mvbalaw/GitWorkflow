0b528eb57b52f3eb2ad2a8057b7c9f6ade19d64b
Clinton Sheppard <csheppard@mvbalaw.com>
2012-02-20 14:14:38 -0600

    automatically determine remote branch

--- scripts/hack.sh
+++ scripts/hack.sh
@@ -1,8 +1,19 @@
-#!/bin/sh -x
-# hack: Merge the latest changes from the master branch into your current branch
-ref=$(git symbolic-ref HEAD 2> /dev/null) || exit 0
-CURRENT="${ref#refs/heads/}"
-git checkout master
-git pull origin master
+#!/bin/sh +x
+# hack: Merge the latest changes from the remote branch into your current branch
+# Git workflow hack script based on: http://reinh.com/blog/2008/08/27/hack-and-and-ship.html
+CURRENT="$(git branch | grep '\*' | awk '{print $2}')"
+echo "current branch is: ${CURRENT}"
+remoteBranches="$(git branch -r | tr '/' ' ' | awk '{print $2}')"
+currentBranches="$(git branch --contains ${CURRENT} | grep -v '\*' | awk '{print $1}')"
+remote="$(echo ${remoteBranches} ${currentBranches} | tr ' ' '\n' | sort | uniq -c | grep "2" | awk '{print $2}' | head -1)"
+echo "remote branch is: ${remote}"
+if [ "${CURRENT}" == "${remote}" -o "" == "${remote}" ] 
+then
+	echo "This command should be run from a feature branch, not from ${CURRENT}."
+	exit 1;
+fi
+set -x
+git checkout $remote
+git pull origin $remote
 git checkout ${CURRENT}
-git rebase master
+git rebase $remote
--- scripts/ship.sh
+++ scripts/ship.sh
@@ -1,9 +1,18 @@
-#!/bin/sh -x
-# Git workflow ship script from: http://reinh.com/blog/2008/08/27/hack-and-and-ship.html
-# git name-rev is fail
-ref=$(git symbolic-ref HEAD 2> /dev/null) || exit 0
-CURRENT="${ref#refs/heads/}"
-git checkout master
+#!/bin/sh +x
+# Git workflow ship script
+CURRENT="$(git branch | grep '\*' | awk '{print $2}')"
+echo "current branch is: ${CURRENT}"
+remoteBranches="$(git branch -r | tr '/' ' ' | awk '{print $2}')"
+currentBranches="$(git branch --contains ${CURRENT} | grep -v '\*' | awk '{print $1}')"
+remote="$(echo ${remoteBranches} ${currentBranches} | tr ' ' '\n' | sort | uniq -c | grep "2" | awk '{print $2}' | head -1)"
+echo "remote branch is: ${remote}"
+if [ "${CURRENT}" == "${remote}" -o "" == "${remote}" ] 
+then
+	echo "This command should be run from a feature branch, not from ${CURRENT}."
+	exit 1;
+fi
+set -x
+git checkout $remote
 git merge ${CURRENT}
-git push origin master
+git push origin $remote
 git checkout ${CURRENT}
