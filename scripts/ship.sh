6868aae1509446df0f521fa73b06bb5cf8a433d4
Clinton Sheppard <csheppard@mvbalaw.com>
2012-02-20 16:39:12 -0600

    pull common functions out to git.lib

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -1,67 +1,12 @@
 #!/bin/sh +x
 # ship: fast forward merge and push your changes to the remote branch
 
-require_clean_work_tree () {
-	# http://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommited-changes
-    # Update the index
-    git update-index -q --ignore-submodules --refresh
-    err=0
-
-    # Disallow unstaged changes in the working tree
-    if ! git diff-files --quiet --ignore-submodules --
-    then
-        echo >&2 "cannot $1: you have unstaged changes."
-        git diff-files --name-status -r --ignore-submodules -- >&2
-        err=1
-    fi
-
-    # Disallow uncommitted changes in the index
-    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
-    then
-        echo >&2 "cannot $1: your index contains uncommitted changes."
-        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
-        err=1
-    fi
-
-    if [ $err = 1 ]
-    then
-        echo >&2 "Please commit or stash them."
-        exit 1
-    fi
+die () {
+    echo >&2 "$@"
+    exit 1
 }
 
-determine_remote_branch () {
-	remoteBranches="$(git branch -r | grep -v 'HEAD' | tr '/' ' ' | awk '{print $2}')"
-	for branch in ${remoteBranches}
-	do
-	    if [ "${CURRENT}" == "${branch}" ] 
-		then
-			remote=""
-			return
-		fi
-	done
-	for branch in ${remoteBranches}
-	do
-		if [ "${CURRENT}" == "$(git branch --contains ${branch} | grep '\*' | awk '{print $2}')" ]
-		then
-			remote=$branch
-			return
-		fi
-	done
-	if [ "" == "${remote}" ] 
-	then
-		currentBranches="$(git branch --contains ${CURRENT} | grep -v '\*' | awk '{print $1}')"
-		remote="$(echo ${remoteBranches} ${currentBranches} | tr ' ' '\n' | sort | uniq -c | grep "2" | awk '{print $2}' | head -1)"
-	fi
-}
-
-assert_working_on_feature_branch () {
-	if [ "${CURRENT}" == "${remote}" -o "" == "${remote}" ] 
-	then
-		echo "This command should be run from a feature branch, not from ${CURRENT}."
-		exit 1;
-	fi
-}
+. "git.lib" || die 'unable to load git.lib library'
 
 require_clean_work_tree
 CURRENT="$(git branch | grep '\*' | awk '{print $2}')"
